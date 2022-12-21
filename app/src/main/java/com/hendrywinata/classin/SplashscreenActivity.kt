9a6110328db1e85378e73ef72b7d2ba4119f36a6
Hendry Winata <hendry24680@gmail.com>
2022-12-21 23:06:56 +0700

    splash screen

---
+++ app/src/main/java/com/hendrywinata/classin/SplashscreenActivity.kt
@@ -0,0 +1,15 @@
+package com.hendrywinata.classin
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
