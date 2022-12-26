659f28e1db79a8c37aab9499d2a0cb8ef741f6be
Hendry Winata <hendry24680@gmail.com>
2022-12-26 22:51:30 +0700

    init Add Course Session 1/2

--- app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
+++ app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hendrywinata.classin.data.CourseItem
-import kotlinx.android.synthetic.main.activity_lecturer_course.*
+import kotlinx.android.synthetic.main.activity_lecturer_course_session.*
 
-class LecturerCourseActivity : AppCompatActivity() {
+class LecturerCourseSessionActivity : AppCompatActivity() {
     private lateinit var course: CourseItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lecturer_course)
+        setContentView(R.layout.activity_lecturer_course_session)
         course = intent.getParcelableExtra("course_detail")!!
         tv_title.text = course.course_name + " " + course.course_class
         btn_back.setOnClickListener { this.finish() }
-        students_presence.setOnClickListener {
+        add_session.setOnClickListener {
             startActivity(
-                Intent(this@LecturerCourseActivity, LecturerCourseSessionActivity::class.java)
+                Intent(this@LecturerCourseSessionActivity, AddCourseSessionActivity::class.java)
                     .putExtra("course_detail", course)
             )
         }
