223c76147b87271252db1349d1f15166c02498ef
Hendry Winata <hendry24680@gmail.com>
2022-12-27 23:21:14 +0700

    Lecturer Course Session Table filled

--- app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
+++ app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
@@ -3,8 +3,16 @@ package com.hendrywinata.classin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TableRow
+import android.widget.TextView
 import com.hendrywinata.classin.data.CourseItem
+import com.hendrywinata.classin.data.PresenceDetail
+import com.hendrywinata.classin.rest.RetrofitClient
 import kotlinx.android.synthetic.main.activity_lecturer_course_session.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LecturerCourseSessionActivity : AppCompatActivity() {
     private lateinit var course: CourseItem
@@ -20,6 +28,50 @@ class LecturerCourseSessionActivity : AppCompatActivity() {
                 Intent(this@LecturerCourseSessionActivity, AddCourseSessionActivity::class.java)
                     .putExtra("course_detail", course)
             )
+        }
+
+        RetrofitClient.instance.getPresencesByCourseID(course.course_id)
+            .enqueue(object: Callback<ArrayList<PresenceDetail>> {
+                override fun onResponse(
+                    call: Call<ArrayList<PresenceDetail>>,
+                    response: Response<ArrayList<PresenceDetail>>
+                ) {
+                    if (response.code() == 200) {
+                        val list = response.body()
+                        if (list!!.isEmpty()) {
+                            tv_text.text = "No Presence Sessions Available"
+                        } else {
+                            buildPresenceList(list)
+                        }
+                    } else {
+                        tv_text.text = "Fail fetching from database"
+                        Log.d("GET PRESENCES FAIL ${response.code()}", response.body().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<PresenceDetail>>, t: Throwable) {
+                    tv_text.text = "Fail fetching from database..."
+                    Log.d("GET PRESENCES FAIL", t.toString())
+                }
+            })
+    }
+
+    private fun buildPresenceList(list: ArrayList<PresenceDetail>) {
+        for (detail in list) {
+            val row = TableRow(this)
+            val idTV = TextView(this)
+            idTV.text = detail.id
+            val startTV = TextView(this)
+            startTV.text = detail.start
+            val endTV = TextView(this)
+            endTV.text = detail.start
+            val descTV = TextView(this)
+            descTV.text = detail.status + "\n" + detail.description
+            row.addView(idTV)
+            row.addView(startTV)
+            row.addView(endTV)
+            row.addView(descTV)
+            session_table.addView(row)
         }
     }
 }
---
+++ app/src/main/java/com/hendrywinata/classin/data/PresenceDetail.kt
@@ -0,0 +1,13 @@
+package com.hendrywinata.classin.data
+
+data class PresenceDetail(
+    val id: String?,
+    val course_id: String?,
+    val start: String?,
+    val end: String?,
+    val status: String?,
+    val location: String?,
+    val lecturer_location: String?,
+    val lecturer_datetime: String?,
+    val description: String?
+)
