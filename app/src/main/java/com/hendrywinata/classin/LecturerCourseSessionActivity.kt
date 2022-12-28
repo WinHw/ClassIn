48ebd004b136ff8a578de2d49f88dcdf32c0ee91
Hendry Winata <hendry24680@gmail.com>
2022-12-28 10:57:21 +0700

    format datetime string and customize tablelayout

--- app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
+++ app/src/main/java/com/hendrywinata/classin/LecturerCourseSessionActivity.kt
@@ -1,11 +1,14 @@
 package com.hendrywinata.classin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.TableRow
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.hendrywinata.classin.data.CourseItem
 import com.hendrywinata.classin.data.PresenceDetail
 import com.hendrywinata.classin.rest.RetrofitClient
@@ -13,9 +16,13 @@ import kotlinx.android.synthetic.main.activity_lecturer_course_session.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LecturerCourseSessionActivity : AppCompatActivity() {
     private lateinit var course: CourseItem
+    private val datetimeFormat = "yyyy-MM-dd HH:mm:ss"
+    private val datetimeFormatter = SimpleDateFormat("EEE, d MMM\nyyyy (HH:mm)", Locale.getDefault())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +48,7 @@ class LecturerCourseSessionActivity : AppCompatActivity() {
                         if (list!!.isEmpty()) {
                             tv_text.text = "No Presence Sessions Available"
                         } else {
+                            tv_text.visibility = View.INVISIBLE
                             buildPresenceList(list)
                         }
                     } else {
@@ -59,19 +67,47 @@ class LecturerCourseSessionActivity : AppCompatActivity() {
     private fun buildPresenceList(list: ArrayList<PresenceDetail>) {
         for (detail in list) {
             val row = TableRow(this)
-            val idTV = TextView(this)
-            idTV.text = detail.id
-            val startTV = TextView(this)
-            startTV.text = detail.start
-            val endTV = TextView(this)
-            endTV.text = detail.start
-            val descTV = TextView(this)
-            descTV.text = detail.status + "\n" + detail.description
-            row.addView(idTV)
+            row.apply {
+                gravity = Gravity.CENTER
+            }
+            val presenceStatus = detail.status!!.uppercase()
+//            val statusTV =
+//                if (presenceStatus == "OPENED") buildButtonTo(3F, presenceStatus, detail.id.toString())
+//                else buildTextView(3F, presenceStatus)
+            val statusTV = buildTextView(3F, presenceStatus)
+            val startTV = buildTextView(3F, datetimeFormatter.format(SimpleDateFormat(datetimeFormat).parse(detail.start)))
+            val endTV = buildTextView(3F, datetimeFormatter.format(SimpleDateFormat(datetimeFormat).parse(detail.end)))
+            val descTV = buildTextView(3F, detail.description.toString())
+            row.addView(statusTV)
             row.addView(startTV)
             row.addView(endTV)
             row.addView(descTV)
             session_table.addView(row)
         }
+    }
+
+    private fun buildTextView(weight: Float, text: String): TextView {
+        val tv = TextView(this)
+        tv.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        tv.textSize = 12F
+        tv.text = text
+        tv.layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.WRAP_CONTENT,
+            weight
+        )
+        return tv
+    }
+
+    private fun buildButtonTo(weight: Float, text: String, refID: String): Button {
+        val btn = Button(this)
+        btn.text = text
+        btn.textSize = 12F
+        btn.layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT,
+            weight
+        )
+        return btn
     }
 }
